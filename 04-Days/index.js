a0adaa425633c1d40c6402e5b474d31faa22c033
Arda Şalvarlılar <93258286+ardasalvarlilar@users.noreply.github.com>
2023-06-19 19:10:08 +0300

    Add files via upload

---
+++ 04-Days/index.js
@@ -0,0 +1,94 @@
+// let userInput = prompt('What is your age?')
+// if(userInput >= 18){
+//   console.log('You are old enough to drive')
+// }else{
+//   console.log(`you can drive ${18 - userInput} years later`)
+// }
+
+// let myAge = 21
+// let yourAge = prompt('What is your age?')
+// console.log('your age is: ' + yourAge)
+// if(yourAge > myAge){
+//   console.log(`you are ${yourAge - myAge} years older than me`)
+// } else{
+//   console.log(`i am ${myAge - yourAge} years older than you`)
+// }
+let a = 4
+let b = 3
+
+a - b ? console.log('a is greater than b') : console.log('b is greater than a')
+if(a > b){
+  console.log('a is greater than b')
+}else{
+  console.log('b is greater than a')
+}
+
+// let userInput = prompt('enter a number')
+// console.log(userInput)
+// if(userInput % 2 == 0){
+//   console.log(`${userInput} is an even number`)
+// }else{
+//   console.log(`${userInput} is an odd number`)
+// }
+
+// let grade = prompt('What is your grate')
+
+// if(grade >= 90 && grade <= 100){
+//   console.log('A')
+// }else if(grade < 90 && grade >= 70){
+//   console.log('B')
+// }else if(grade < 70 && grade >= 60){
+//   console.log('C')
+// }else if(grade < 60 && grade >= 50){
+//   console.log('D')
+// }else{
+//   console.log('F')
+// }
+
+// let month = prompt('what is the month').toLowerCase()
+// if(month == 'october' || month == 'november' || month == 'september'){
+//   console.log('You are in Autumn')
+// }else if(month == 'december' || month == 'january' || month == 'february'){
+//   console.log('You are in Winter')
+// }else if(month == 'march' || month == 'april' || month == 'may'){
+//   console.log('You are in Spring')
+// }else{
+//   console.log('You are in summer')
+// }
+
+// let day = prompt('day')
+
+// if(day == 'saturday' || day == 'sunday'){
+//   console.log(`${day} is weekend`)
+// }else{
+//   console.log(`${day} is work day`)
+// }
+
+// let input = prompt('month')
+// let months = ['january','february','march','april','may','june','july','august','september','october','november','december']
+// if(input == months[0]){
+//   console.log(`${input} has 31 days`)
+// }else if(input == months[1]){
+//   console.log(`${input} has 28 days`)
+// }else if(input == months[2]){
+//   console.log(`${input} has 31 days`)
+// }else if(input == months[3]){
+//   console.log(`${input} has 30 days`)
+// }else if(input == months[4]){
+//   console.log(`${input} has 31 days`)
+// }else if(input == months[5]){
+//   console.log(`${input} has 30 days`)
+// }else if(input == months[6]){
+//   console.log(`${input} has 31 days`)
+// }else if(input == months[7]){
+//   console.log(`${input} has 30 days`)
+// }else if(input == months[8]){
+//   console.log(`${input} has 31 days`)
+// }else if(input == months[9]){
+//   console.log(`${input} has 30 days`)
+// }else if(input == months[10]){
+//   console.log(`${input} has 31 days`)
+// }else if(input == months[11]){
+//   console.log(`${input} has 30 days`)
+// }
+
